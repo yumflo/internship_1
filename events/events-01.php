39d554fc4fdac5e2cf4d24ea4694d84d0b6c38e3
yumflo <kisscry225@gmail.com>
2015-06-25 20:51:25 +0800

    OK

---
+++ events/events-01.php
@@ -0,0 +1,262 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="zh-tw">
+<head>
+<meta name="Generator" content="EditPlus" />
+<meta name="Author" content="FT" />
+<meta name="Keywords" content="" />
+<meta name="Description" content="" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<title>兆豐文創 Harvest Education</title>
+<style type="text/css">
+
+<!--
+a { text-decoration: none } 
+a:link {color: #FFF}
+a:visited {color:#FFF}
+a:active {color:#ffffff}
+a:hover {color: #FC9
+}
+a:hover img{
+ opacity: 0.55;
+ filter:alpha(opacity=55); /* IE only */}
+-->
+
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-bottom: 0px;
+	background-image: url();
+}
+
+	#menu {
+	margin: 0;
+	padding: 0;
+	list-style: none;
+	position: relative;
+	float: left;
+	background: #fff;
+	font-family: Helvetica,Microsoft JhengHei,Apple LiGothic, Arial, sans-serif;
+	font-size:10.5pt;
+	}
+	#menu li {
+		margin: 0px;
+		padding: 0px;
+		float: left;
+		border-right: 0px solid #999;
+	}
+	#menu li a {
+		padding: 3px 10px;
+		display: block;
+		color: #000;
+		text-decoration: none;
+	}
+	#menu li ul {
+		margin: 0;
+		padding: 6px 20px;
+		list-style: none;
+		float: left;
+		position: absolute;
+		line-height:22px;
+		left: 0;
+		width: 500px;
+		color: #fff;
+		background: # ;
+		display: none;
+	}
+	#menu li ul li { border: 1px solid #fff; }
+	#menu li ul li a { display: inline; }
+	#menu li ul li a:hover { color:#eb6123; }
+	#menu li a:hover { color:#45aeaa; }
+	
+.brown {
+	text-align: left;
+	font-size:9pt;
+	color:#FFF;
+	font-family: Arial, Helvetica, sans-serif;
+	line-height:10pt;
+}
+.button {	font-size:11pt; color:#332e2e; #; font-family: Helvetica, sans-serif; line-height:12pt; letter-spacing:1pt;
+}
+.info {
+	font-size:10pt;
+	color:#666;
+#; 	font-family:Helvetica, Microsoft JhengHei, Apple LiGothic, Arial, sans-serif;
+	line-height:12pt;
+	letter-spacing:1px;
+	text-align: center; 
+}
+.more {	font-style:oblique ;font-size:10pt; font-family: Georgia, "Times New Roman", Times, serif; line-height:18pt; color:#464141; font-weight:500; letter-spacing:1pt;
+}
+.info1 {font-size:10pt; color:#333; #; font-family:Helvetica, Microsoft JhengHei, Apple LiGothic, Arial, sans-serif; line-height:12pt; letter-spacing:1px;
+}
+.chtitle {
+	font-size:15px;
+	color:#333;
+	font-family:Helvetica, Microsoft JhengHei, Apple LiGothic, Arial,  sans-serif;
+	letter-spacing:1px;
+	font-style: normal;
+	text-align: center;
+	line-height:20px;
+}
+
+.entitle {
+	font-size:13px;
+	color:#333;
+	font-family:Helvetica, Microsoft JhengHei, Apple LiGothic, Arial,  sans-serif;
+	letter-spacing:1px;
+	font-style: normal;
+	text-align: center;
+	line-height:20px;
+}
+.title {	font-size:25px;
+	color: #CCC;
+#; 	font-family:Georgia, "Times New Roman", Times, serif;
+	letter-spacing:1px;
+	font-style: italic;
+}
+</style>
+<script type="text/javascript">
+$(function(){
+		// 幫 #menu li 加上 hover 事件
+		$('#menu>li').hover(function(){
+			// 先找到 li 中的子選單
+			var _this = $(this),
+				_subnav = _this.children('ul');
+			
+			// 變更目前母選項的背景顏色
+			// 同時淡入子選單(如果有的話)
+			_this.css('backgroundColor', '#');
+			_subnav.stop(true, true).fadeIn(400);
+		} , function(){
+			// 變更目前母選項的背景顏色
+			// 同時淡出子選單(如果有的話)
+			// 也可以把整句拆成上面的寫法
+			$(this).css('backgroundColor', '').children('ul').stop(true, true).fadeOut(400);
+		});
+		
+		// 取消超連結的虛線框
+		$('a').focus(function(){
+			this.blur();
+		});
+	});
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+<link href="layout.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" href="icon/favicon.ico" >
+<link rel="Bookmark" href="icon/favicon.ico" type="image/x-icon" />
+</head>
+
+<body onLoad="MM_preloadImages('photo/home2.png','photo/login2.png')">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="100%" height="30" align="right" valign="middle" bgcolor="#1FBD70" ><table border="0" cellpadding="0" cellspacing="0">
+      <tr>
+         <td align="center" valign="middle"><span class="brown"><a href="file:///Macintosh HD/Users/macintosh/Desktop/Harvest Education 2/home2.html"></a><a href="http://www.harvesteducation.cc/" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image13','','icon/home2.png',1)"><img src="icon/home.png" alt="" name="Image13" height="11" border="0"></a>　<a href="http://alchemytechnology.no-ip.org:4050/BrainwaveClass/index/login.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image12','','icon/login2.png',1)"><img src="icon/login.png" alt="" name="Image12" width="58" height="11" border="0"></a></span></td>
+        <td width="10">&nbsp;</td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+<table width="950" border="0" align="center" cellpadding="0" cellspacing="0">
+<tr>
+	    <td width="950"><table width="950" border="0" cellpadding="0" cellspacing="0">
+	      <tr>
+	        <td height="30">&nbsp;</td>
+	        <td>&nbsp;</td>
+          </tr>
+	      <tr>
+	        <td valign="middle"><a href="http://www.harvesteducation.cc" target="mainFrame"><img src="photo/Logo-large.png" alt="" height="65" border="0" /></a></td>
+	        <td><table border="0" align="right" cellpadding="0" cellspacing="0">
+	          <tr>
+	            <td width="" height="15" valign="top"><ul id="menu">
+	              <li> <a href="#">News</a>
+	                <ul>
+	                  <li><a href="http://www.harvesteducation.cc/News/news.php">最新消息</a></li>
+	                  <li><a href="#">最新趨勢</a></li>
+	                  </ul>
+	                <li><a href="#">About Us</a>
+	                  <ul>
+	                    <li><a href="#">關於我們</a></li>
+	                    <li><a href="http://www.harvesteducation.cc/media/media.php">媒體報導</a></li>
+	                    <li><a href="http://www.harvesteducation.cc/contact.html" >聯絡我們</a></li>
+	                    <li><a href="#">誠聘英才</a></li>
+                      </ul>
+                    <li><a href="#">Brainwave Education</a>
+	                    <ul>
+	                      <li><a href="#">21天腦波雲端教室</a></li>
+	                      <li><a href="#">音樂療癒課程</a></li>
+	                      <li><a href="#">Pad 腦波學習系統</a></li>
+	                      <li><a href="http://www.harvesteducation.cc/zenlamp.html" target="mainFrame">Zen Lamp 智慧生活放鬆燈</a></li>
+	                      <li><a href="#">BrainCubed 專精學習設計平台</a></li>
+                      </ul>
+                    </li>
+	              <li><a href="#">Partners</a></li>
+	              <li><a href="#">Downloads</a><a href="#"></a></li>
+	              <li><a href="#">Join us</a></li>
+                </ul></td>
+              </tr>
+            </table></td>
+          </tr>
+	      </table></td>
+  </tr>
+	  <tr>
+	    
+      </tr>
+	  <tr>
+	    <td><p>&nbsp;</p>
+		</td>
+      </tr>
+</table>
+<table width="950" border="0" align="center" cellpadding="0" cellspacing="0">
+
+<div id="WRAPPER">
+  <div id="CONTENT">
+    <div class="BOX">
+    <h2>※尋找專心的音符　勤益科大同步腦波實驗室有答案  </h2>
+    <p><font size="3">
+	<a href="http://w3.iiiedu.org.tw/ilc/rainbowfun/science.php#01"><font color="red">活動網址http://w3.iiiedu.org.tw/ilc/rainbowfun/science.php#01</font></a><br>
+「學習要有動力才能成長，創意要靠實踐才有價值。」因著這兩句話，資策會的數位教育研究所和智高實業、大寶科技和兆豐文創合作，舉辦了一場為期五天，與科普同樂的科技盛會，讓孩子沉浸在求知的海洋中，尋找自己的一片廣闊天空。<br>
+為期五天的課程裡，每個時刻都能讓孩子有滿滿的收穫。每天都有豐富的課程和專業的講師，讓孩子學會物理與化學的專業知識；透過分組行動與團隊合作，每個孩子都能學會和他人溝通與表達；藉由實際練習與操作，孩子不僅吸收了知識，更將知識留存在經驗中。<br>
+認識了自己的大腦，學會控制自己的專注與放鬆力，孩子們藉由大寶科技所提供的腦波儀看見自己的狀態在螢幕上呈現出來，驚訝之餘更想學會如何操控。再看看腦波放鬆燈所變化的顏色，孩子慢慢地學會專注地學習和身心靈的放鬆，等到訓練一定程度，重頭戲就要來囉！<br>
+為了讓團隊所訓練的車子能順利地快速前進，孩子們費盡心思去思考和討論，打造出最輕最帥最時髦的樂高車；再選出最能控制自己腦波專注力的人選，比賽就此開始！每個人的專注程度在眉頭上可知，有的人不到十秒便笑了出來，也有人可以讓帥氣的樂高車走完全程，專注的程度高人一等！
+八月還有一場在台中舉辦的科學營，兩天的活動一樣精彩萬分，歡迎點進網址了解，名額有現請盡速報名！<br>
+※動動「腦」－機器人科學營－台中場次<br>
+
+<a href="http://w3.iiiedu.org.tw/ilc/rainbowfun/scienceTaichung_2.php#01"><font color="red">活動網址http://w3.iiiedu.org.tw/ilc/rainbowfun/scienceTaichung_2.php#01</font></a><br>
+</font>
+ </p>
+    </div>
+  </div>
+</div>
+  <tr>
+    <td width="500" align="center" valign="middle"><span class="info">兆豐文創有限公司               / 404   北區 博館路89號6樓之2                     / tel. 04-23292926                   / fax. 04-23922875 </span></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
